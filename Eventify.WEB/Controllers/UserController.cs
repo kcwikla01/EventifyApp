25c2da01ba373af8d88babfee164f4f932aa6373
Kamil Cwikła <k.cwikla@medidok.de>
2025-03-12 18:02:53 +0100

    Create user controller

---
+++ Eventify.WEB/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventify.WEB.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        
+    }
+}
