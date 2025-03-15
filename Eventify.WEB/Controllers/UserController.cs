b44cdf41c47d42636c1480f17830da42a0550e4a
Kamil Cwikla <kamilcwikla01@gmail.com>
2025-03-15 20:49:03 +0100

    Configure Autofac dependency injection Create UserDto Create UserApplicationService Create UserController with endpoint for create User

--- Eventify.WEB/Controllers/UserController.cs
+++ Eventify.WEB/Controllers/UserController.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Eventify.Database.Models.Dto;
+using Eventify.WEB.ApplicationServices.Base;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eventify.WEB.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class UserController : Controller
     {
-        
+        private readonly IUserApplicationService _userApplicationService;
+
+        public UserController(IUserApplicationService userApplicationService)
+        {
+            _userApplicationService = userApplicationService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserDto userDto)
+        {
+            return _userApplicationService.CreateUser(userDto);
+        }
     }
 }
